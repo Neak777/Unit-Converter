edde267d4598085dd8b2d8550815c0305a1d8523
Neak777 <121606310+Neak777@users.noreply.github.com>
2023-03-28 13:00:20 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+
+convertBtn.addEventListener("click", function() {
+    let baseValue = inputEl.value
+    
+   
+    
+    
+     lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet`
+  lengthEl.textContent += ` | ${baseValue} feet = ${baseValue / meterToFeet} meters`
+
+  volumeEl.textContent = `${baseValue} liters = ${baseValue * literToGallon} gallons`
+  volumeEl.textContent += ` | ${baseValue} gallons = ${baseValue / literToGallon} liters`
+
+  massEl.textContent = `${baseValue} kilograms = ${baseValue * kiloToPound} pounds`
+  massEl.textContent += ` | ${baseValue} pounds = ${baseValue / kiloToPound} kilograms`
+
+     
+})
+
+
+
+
+
+
